d83744b24be8c417535ba4f5192d01bbe8295e04
704167880 <704167880@qq.com>
2016-12-01 15:38:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+#TinkerDemo
+https://github.com/Tencent/tinker 微信热修复解决方案
--- app/src/main/java/com/jin/tinkerdemo/MainActivity.java
+++ app/src/main/java/com/jin/tinkerdemo/MainActivity.java
@@ -1,13 +1,45 @@
 package com.jin.tinkerdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+import java.io.File;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @BindView(R.id.tv)
+    TextView mTv;
+    @BindView(R.id.btn)
+    Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        //mTv.setText("old");
+        //mTv.setText("再次修复");
+        mTv.setText("new");//修复后的bug
+        mBtn.setOnClickListener(view -> {
+            String patchPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tinkersample/patch_signed_7zip.apk";
+            File file = new File(patchPath);
+            if (file.exists()) {
+                Log.v(TAG, "补丁文件存在");
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), patchPath);
+            } else {
+                Log.v(TAG, "补丁文件不存在");
+            }
+
+        });
+
     }
 }
